0bccc1c5de2c9ac0404b2121b085475e69480b9c
Leela manoj Mallela <36662524+leelamanoj06@users.noreply.github.com>
2020-05-08 09:58:52 +0530

    Test code pushed to repository.

---
+++ Its result time/Main.java
@@ -0,0 +1,8 @@
+#include<iostream>
+int main()
+{
+  int a,b;
+  std::cin>>a;
+  std::cin>>b;
+  std::cout<<"Id : "<<a<<"\nTotalmarks : "<<b;
+}  
---
+++ Raj in wonderland/Main.java
@@ -0,0 +1,15 @@
+#include<iostream>
+int main()
+{
+  int n;
+  std::cin>>n;
+  if(n%2==0)
+  {
+    std::cout<<"Possible";
+  }
+  else
+  {
+    std::cout<<"Not possible";
+  }
+  
+}
